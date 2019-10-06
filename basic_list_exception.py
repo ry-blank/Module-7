54cd35a1995587c093c135e0825e0f1ce70a166c
ry-blank <52143833+ry-blank@users.noreply.github.com>
2019-10-06 18:52:25 -0500

    Basic List Exception Assignment
    Initial commit - Failing tests - ValueError not raised

---
+++ basic_list_exception.py
@@ -0,0 +1,38 @@
+"""
+Program: basic_list_exception_assignment
+Author: Ryan Blankenship
+Last Date Modified: 10/6/2019
+
+The purpose of this program is to take user
+input then display it in a list.
+"""
+
+
+def make_list():
+    """
+    function to return list of user input from function get_input()
+    :return: returns list of user input
+    """
+    number_list = []
+    attempts = 3
+    for a in range(attempts):
+        try:
+            user_num = int(get_input())
+        except ValueError:
+            print("Please enter numbers only.")
+        else:
+            number_list.insert(len(number_list), user_num)
+    return number_list
+
+
+def get_input():
+    """
+    function to prompt user for input
+    :return: returns a string
+    """
+    user_num = input("Please enter a number to add to your list: ")
+    return user_num
+
+
+if __name__ == '__main__':
+    print(make_list())
