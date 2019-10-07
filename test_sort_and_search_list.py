b14c798d3192ec2cd5505019f6ed272ef13217c9
ry-blank <52143833+ry-blank@users.noreply.github.com>
2019-10-07 18:11:47 -0500

    Search and Sort List Assignment
    3rd commit - test for sort_list() failed.

--- test_sort_and_search_list.py
+++ test_sort_and_search_list.py
@@ -9,6 +9,9 @@ class SearchList(unittest.TestCase):
     def test_search_list_for_item__not_found(self):
         self.assertEqual(sort_and_search_list.search_list([5, 10, 15, 20, 25], 30), -1)
 
+    def test_sort_list(self):
+        self.assertEqual(sort_and_search_list.sort_list([5, 25, 10, 20, 15]), [0])
+
 
 if __name__ == '__main__':
     unittest.main()
