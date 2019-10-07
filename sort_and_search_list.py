00b1ba97ce186d6219cf44a1149e5430a0047e89
ry-blank <52143833+ry-blank@users.noreply.github.com>
2019-10-07 17:21:25 -0500

    Search and Sort List Assignment
    Initial commit - both search_list() tests failing.

---
+++ sort_and_search_list.py
@@ -0,0 +1,32 @@
+"""
+Program: search_and_sort_list_assignment
+Author: Ryan Blankenship
+Last Date Modified: 10/7/2019
+
+The purpose of this program is to sort a list
+or search a list for item then return index or
+ValueError if item not found.
+"""
+
+
+def sort_list(a_list):
+    """
+    function to sort list of user input
+    :param a_list: list of user input
+    """
+    pass
+
+
+def search_list(a_list, a_number):
+    """
+    function to search list for item
+    :return: the index of item in list or item not found
+    :param a_list: list from user input
+    :param a_number: to search index
+    :return: the index of item in list or item not found
+    """
+    pass
+
+
+if __name__ == '__main__':
+    sort_list()
---
+++ test_sort_and_search_list.py
@@ -0,0 +1,14 @@
+import unittest
+from fun_with_collections import sort_and_search_list
+
+
+class SearchList(unittest.TestCase):
+    def test_search_list_for_item_found(self):
+        self.assertEqual(sort_and_search_list.search_list([5, 10, 15, 20, 25], 25), 4)
+
+    def test_search_list_for_item__not_found(self):
+        self.assertEqual(sort_and_search_list.search_list([5, 10, 15, 20, 25], 30), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
